7b83263aa197b56b6927c7eb30eb4fe3b320c5c5
Sergey <sergey@MacBook-Pro-de-Sergey.local>
2025-05-29 11:39:13 +0200

    added handleCellClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,57 @@
-import { useState } from "react";
-import "./App.css";
-import Cell from "./components/Cell";
+import { useState } from 'react';
+import './App.css';
+import Cell from './components/Cell';
 
 export default function App() {
-  const boardSize = 10;
+	const boardSize = 10;
 
-  const emptyBoard = Array(boardSize)
-    .fill(null)
-    .map(() => {
-      return Array(boardSize)
-        .fill(null)
-        .map(() => ({
-          value: null,
-        }));
-    });
+	const emptyBoard = Array(boardSize)
+		.fill(null)
+		.map(() => {
+			return Array(boardSize)
+				.fill(null)
+				.map(() => ({
+					value: null,
+				}));
+		});
 
-  const [board, setBoard] = useState(emptyBoard);
-  const [step, setStep] = useState("x");
+	const [board, setBoard] = useState(emptyBoard);
+	const [step, setStep] = useState('x');
 
-  const handleCellClick = (i, j) => {
-    console.log(i, j);
-  };
+	const handleCellClick = (i, j) => {
+		setBoard(
+			board.map((row, rowIndex) => {
+				if (rowIndex === i) {
+					return row.map((cell, cellIndex) => {
+						if (cellIndex === j && cell.value === null) {
+							return { value: step };
+						} else {
+							return cell;
+						}
+					});
+				} else {
+					return row;
+				}
+			})
+		);
+		setStep(step === 'x' ? 'o' : 'x');
+	};
 
-  return (
-    <div id="board-container" className={`step-${step}`}>
-      {board.map((row, rowIndex) => (
-        <div className="row" key={rowIndex}>
-          {row.map((cell, cellIndex) => (
-            <Cell
-              key={cellIndex}
-              value={cell.value}
-              onClick={handleCellClick}
-              rowIndex={rowIndex}
-              cellIndex={cellIndex}
-            />
-          ))}
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div id='board-container' className={`step-${step}`}>
+			{board.map((row, rowIndex) => (
+				<div className='row' key={rowIndex}>
+					{row.map((cell, cellIndex) => (
+						<Cell
+							key={cellIndex}
+							value={cell.value}
+							onClick={handleCellClick}
+							rowIndex={rowIndex}
+							cellIndex={cellIndex}
+						/>
+					))}
+				</div>
+			))}
+		</div>
+	);
 }
